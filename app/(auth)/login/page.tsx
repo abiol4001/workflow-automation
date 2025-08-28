c5debfc08d1f3dcf59845f6cc0c0c8a189ca4830
abiol4001 <abiol4001@gmail.com>
2025-08-28 20:41:11 +0100

    first commit

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import SigninForm from '@/components/SignInForm'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+          <SigninForm />
+        </div>
+  )
+}
+
+export default Login
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import SignupForm from '@/components/SignUpForm'
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <SignupForm />
+    </div>
+  )
+}
+
+export default SignUp
